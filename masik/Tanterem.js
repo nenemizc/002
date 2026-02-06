1ce817d3a4c9407b75586c07c930744f3e3b846d
nenemizc <emarcellm@gmail.com>
2026-02-06 14:10:26 +0100

    class

--- masik/Tanterem.js
+++ masik/Tanterem.js
@@ -1,1 +1,11 @@
-class Tanterem{}
+export default class Tanterem{
+    constructor(epulet, sorszam, osztaly){
+        this.epulet = epulet;
+        this.sorszam = sorszam;
+        this.osztaly = osztaly;
+    }
+
+    ToString(){
+        return `${this.epulet}, ${this.sorszam}, ${this.osztaly}`;
+    }
+}
