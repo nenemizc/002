9a75081789f91717799cc1505a3e74a42fd93b63
nenemizc <emarcellm@gmail.com>
2026-02-06 14:15:26 +0100

    sort hozáadva az elsőhöz

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ rl.question("Adja meg az adatok számát: ", (answer) => {
     let darab = answer;
     for (let i = 0; i < darab; i++){
         let adat = Math.floor(Math.random() * (200 - 150 + 1) + 150);
-        console.log(adat);
         magassagok.push(adat);
+        magassagok = magassagok.sort();
     }
     
     for (let i = 0; i < magassagok.length;i++){
-        // console.log(magassagok[i]);
+        console.log(magassagok[i]);
         fs.writeFileSync('./magassagok.txt',magassagok[i].toString() + '\n',{ flag: 'a' });
     }
     rl.close();
